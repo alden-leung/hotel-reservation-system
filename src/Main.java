791e31764fec8c19991d292ede65e40eb846c0f3
NIMSHI <140698131+nim-shi@users.noreply.github.com>
2025-11-29 21:59:09 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,7 @@ public class Main {
 
     public static void main(String[] args) {
         //TODO: User Interface
+        checkIn();
     }
 
     /*
@@ -79,11 +80,47 @@ public class Main {
     1. Input name of walk-in guest
     2. Input what type of room they will get
     3. Input number of nights booked
-    4. Verify if there is room availability and update room status to occupied
-    5. Display check-in successful with guest name and room number
+    4. Verify if there is room availability
+    5. Update room status to occupied
+    6. Payment for room
+    7. Display check-in successful with guest name and room number
     */
 
-   
+    // 1. Input name of walk-in guest
+         String guestName = getUserInput("Input Guest Name: ");
+
+    // 2. Input what type of room they will get
+
+        String roomType = "";
+        int bill = 0;
+
+         do {
+            roomType = getUserInput("Input Room Type: (1. Standard, 2. Deluxe, 3. Suite): ");
+
+            switch (roomType) {
+                case "1" -> {checkRoomStatus(standard); bill = 2500; roomType = "Standard";}
+                case "2" -> {checkRoomStatus(deluxe); bill = 4000;roomType = "Deluxe";}
+                case "3" -> {checkRoomStatus(suite); bill = 8000;roomType = "Suite";}
+                default -> {
+                    System.out.println("Invalid room type, try again");
+                    roomType = "False";
+                }
+            }
+
+        } while (roomType.equals("False"));
+
+    // 3. Input of nights booked
+
+        String nBooked = getUserInput("input nights booked:");
+        System.out.println("Processing Walk-in Check-In... Checking for available " + roomType + " rooms"+ "(Php "+bill +"/night)...");
+        
+        
+    // 5. Update room status to occupied
+
+    // 6. Payment for room
+
+    // 7. Display check-in successful with guest name and room number
+
     return false;// just a placeholder to avoid errors
 }
 
@@ -116,9 +153,9 @@ public class Main {
     }
 
     private static String checkRoomStatus(String[][] room) {
-
-        return ""; // just a placeholder to avoid errors
-    }
+    
+    return "No rooms found."; // fallback
+}
 
     private static void printTable() {
         //TODO: print table layout. (using the standard, deluxe, and suite 2D-arrays)
