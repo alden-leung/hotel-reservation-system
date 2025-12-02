5a33d4058414de8e2fdb415c891fb93b3a39d55e
NIMSHI <140698131+nim-shi@users.noreply.github.com>
2025-12-02 20:10:00 +0800

    Update Main.java
    fixed no available rooms for the requested nights

--- src/Main.java
+++ src/Main.java
@@ -345,11 +345,10 @@ public class Main {
                         }
                     }while (payment < bill * numOfNights);
                     break;
-                } 
-            }else{
-                    System.out.println("No available rooms for the requested nights.");
                 }
+            }
         }
+        System.out.println("No available rooms for the requested nights.");
     return false;// just a placeholder to avoid errors
 }
 
